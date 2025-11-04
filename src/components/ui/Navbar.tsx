5741ac84acb9484d0513502228cdc4e40460c1ee
Ariff-dev <ariff.dev24@gmail.com>
2025-11-03 22:02:49 -0600

    feat: Navbar component | Style Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,19 +1,60 @@
+import { ChevronDown, CircleX, LayoutGrid } from 'lucide-react'
+import { useState } from 'react'
+import type { MenuState } from '@interfaces/ui'
+type MenuName = keyof MenuState
+
 export const Navbar = () => {
+  const [openMenus, setOpenMenus] = useState<MenuState>({
+    main: false,
+    comunidad: false,
+    recursos: false,
+    servicios: false,
+  })
+
+  const toggleMenu = (menuName: MenuName): void => {
+    setOpenMenus((prev) => ({
+      ...prev,
+      [menuName]: !prev[menuName],
+    }))
+  }
+
   return (
-    <nav>
-      <img src='#' alt='Logo' />
-      <ul>
-        <li>Inicio</li>
-        <div>
-          <ul>
-            <li>Sobre nostros</li>
-            <li>Objetivos</li>
-            <li>Postular</li>
-            <li>Contacto</li>
-          </ul>
-        </div>
-      </ul>
-      <ul></ul>
+    <nav className=''>
+      <button onClick={() => toggleMenu('main')}>
+        <LayoutGrid width={30} height={30} className='fixed top-0'/>
+      </button>
+      <div className={`${!openMenus.main ? 'hidden' : 'block'} bg-contrast fixed top-0 min-h-screen w-1/2 text-white p-2 flex flex-col gap-4`}>
+        <CircleX onClick={() => toggleMenu('main')}/>
+        <ul className='flex flex-col gap-2'>
+          <li>Inicio</li>
+
+          <div>
+            <p
+              className='flex items-center cursor-pointer'
+              onClick={() => toggleMenu('comunidad')}
+            >
+              Comunidad
+              <span>
+                <ChevronDown
+                  width={25}
+                  height={25}
+                  className={`transition-transform ${
+                    openMenus.comunidad ? 'rotate-180' : ''
+                  } text-complementary`}
+                />
+              </span>
+            </p>
+            <ul className={`${!openMenus.comunidad ? 'hidden' : 'block'} px-4`}>
+              <li>CVs</li>
+            </ul>
+          </div>
+
+          <li>Sobre nosotros</li>
+          <li>Objetivos</li>
+          <li>Postular</li>
+          <li>Contacto</li>
+        </ul>
+      </div>
     </nav>
   )
 }
---
+++ src/interfaces/ui.ts
@@ -0,0 +1,6 @@
+export interface MenuState  {
+  main: boolean
+  comunidad: boolean
+  recursos: boolean
+  servicios: boolean
+}
